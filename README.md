69bc8b82dfcc989c9c7124e0b75064e2b76ab0b1
Henrik Bohlin <henbo928@gmail.com>
2018-12-08 11:49:28 +0100

    Added options for server start

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rust_hdl_vscode
 VS Code integration of [Rust HDL](https://github.com/kraigher/rust_hdl#vhdl-language-server) Language Server
+
+## Usage
--- client/src/extension.ts
+++ client/src/extension.ts
@@ -5,32 +5,67 @@
 * ------------------------------------------------------------------------------------------ */
 'use strict';
 import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import vscode = require('vscode');
+import { workspace, ExtensionContext, window } from 'vscode';
+import fs = require('fs');
+// import exec = require('child_process');
+import util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 import {
     LanguageClient,
     LanguageClientOptions,
-    ServerOptions
+    ServerOptions,
+    DiagnosticSeverity
 } from 'vscode-languageclient';
 
 let client: LanguageClient;
 
-export function activate(context: ExtensionContext) {
+enum LanguageServerBinary {
+    Extension, SystemPath, Docker
+}
 
-    // Get the path to the language server executable
-    const serverExecutableDir = 'server';
-    const serverExecutable = process.platform === 'win32'? 
-        context.asAbsolutePath(path.join(serverExecutableDir, 'vhdl_ls.exe')) :
-        context.asAbsolutePath(path.join(serverExecutableDir, 'vhdl_ls'));
+const extensionName = 'viraHdl';
+const languageServerBinaryName = 'vhdl_ls';
 
-    let serverOptions: ServerOptions = {
-        run: {
-            command: serverExecutable
-        },
-        debug: {
-            command: serverExecutable
-        }
-    };
+export async function activate(context: ExtensionContext) {
+    
+    let config = vscode.workspace.getConfiguration(extensionName);
+    let lsBinary = config.get('languageServerBinary') as keyof typeof LanguageServerBinary;
+    const isWindows = process.platform === "win32";
+
+    let useCargo = config.get('enableCargoServerInstall');
+
+    let serverCommand  = context.asAbsolutePath(path.join('server', 'vhdl_ls'));
+    let serverArgs = [];
+    
+    const cargoCrate = 'vhdl_ls';
+    
+    
+    if (isWindows && lsBinary == "Docker") {
+        window.showWarningMessage("Docker language server not supported on Windows, using system path instead");
+        lsBinary = "SystemPath";
+    }
+    // TODO: If extensions is selected and no vhdl_ls found in /server, use system path
+
+    let serverOptions: ServerOptions;
+    switch(lsBinary) {
+        case "Docker": 
+            serverOptions = await getServerOptionsDocker(); 
+            console.log('Using Docker image for language server');
+            break;
+        
+        case "Extension": 
+            serverOptions = getServerOptionsExtension(context);
+            console.log('Using language server installed with extension');
+            break;
+
+        default: 
+            serverOptions = getServerOptionsDefault();
+            console.log('Using language server from system path');
+            break;
+    }
+
 
     // Options to control the language client
     let clientOptions: LanguageClientOptions = {
@@ -41,10 +76,10 @@ export function activate(context: ExtensionContext) {
         }
     };
 
-    // Create the language client and start the client.
+    // Create the language client
     client = new LanguageClient(
-        'languageServerExample',
-        'Language Server Example',
+        'rustHDL',
+        'Rust HDL (VHDL)',
         serverOptions,
         clientOptions
     );
@@ -58,4 +93,72 @@ export function deactivate(): Thenable<void> | undefined {
         return undefined;
     }
     return client.stop();
+}
+
+
+async function getServerOptionsDocker() {
+    const dockerImage = 'kraigher/vhdl_ls:latest';
+    const { stdout, stderr } = await exec("docker pull " + dockerImage);
+    console.log(stdout);
+    console.log(stderr);
+    let serverCommand = 'docker';
+    let serverArgs = ['run', '-i', '-a', 'stdin', '-a', 'stdout', '-a', 'stderr', '--rm'];
+    let wsFolders : any = vscode.workspace.workspaceFolders;
+    let pwdIsSet = false;
+    if (wsFolders) {
+        wsFolders.forEach(path => {
+            let folder : vscode.WorkspaceFolder = (path as vscode.WorkspaceFolder);
+            console.log(folder.uri);
+            let fsPath = folder.uri.path;
+            let mountPath = folder.uri.path;
+            if (!pwdIsSet) {
+                pwdIsSet = true;
+                serverArgs.push('-w');
+                serverArgs.push(mountPath);
+            }
+            serverArgs.push('-v');
+            serverArgs.push(fsPath + ':' + mountPath + ':ro');
+            
+        });
+    }
+    serverArgs.push(dockerImage);
+    console.log(serverCommand);
+    console.log(serverArgs);
+    let serverOptions: ServerOptions = {
+        run: {
+            command: serverCommand,
+            args   : serverArgs
+        },
+        debug: {
+            command: serverCommand,
+            args   : serverArgs
+        }
+    };
+    return serverOptions;
+}
+
+function getServerOptionsExtension(context: ExtensionContext) {
+    let serverCommand = context.asAbsolutePath(path.join('server', languageServerBinaryName));
+    let serverOptions: ServerOptions = {
+        run: {
+            command: serverCommand
+        },
+        debug: {
+            command: serverCommand
+        }
+    };
+    return serverOptions;
+}
+
+function getServerOptionsDefault() {
+    let serverCommand = languageServerBinaryName;
+    let serverOptions: ServerOptions = {
+        run: {
+            command: serverCommand
+        },
+        debug: {
+            command: serverCommand
+        }
+    };
+    return serverOptions;
 }
