469ecd913c3c61abdaa175fae12f4b45cf392492
Henrik Bohlin <henbo928@gmail.com>
2019-12-06 21:01:13 +0100

    Added prettier for formatting

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,8 +1,8 @@
 /* --------------------------------------------------------------------------------------------
-* MIT License
-* Copyright (c) 2019 Henrik Bohlin
-* Full license text can be found in /LICENSE or at https://opensource.org/licenses/MIT.
-* ------------------------------------------------------------------------------------------ */
+ * MIT License
+ * Copyright (c) 2019 Henrik Bohlin
+ * Full license text can be found in /LICENSE or at https://opensource.org/licenses/MIT.
+ * ------------------------------------------------------------------------------------------ */
 'use strict';
 import * as path from 'path';
 import vscode = require('vscode');
@@ -15,72 +15,75 @@ import {
     LanguageClient,
     LanguageClientOptions,
     ServerOptions,
-    DiagnosticSeverity
+    DiagnosticSeverity,
 } from 'vscode-languageclient';
 
 let client: LanguageClient;
 
 enum LanguageServerBinary {
-    embedded, user, systemPath, docker
+    embedded,
+    user,
+    systemPath,
+    docker,
 }
 
-const HOMEDIR = require('os').homedir().replace(/\\/g, "/");
+const HOMEDIR = require('os')
+    .homedir()
+    .replace(/\\/g, '/');
 const CONFFILE = 'vhdl_ls.toml';
 const HOMECONF = path.join(HOMEDIR, '.' + CONFFILE);
 const languageServerBinaryName = 'vhdl_ls';
-const isWindows = process.platform === "win32";
+const isWindows = process.platform === 'win32';
 
 export async function activate(context: ExtensionContext) {
-
     // Default configuration
     if (!fs.existsSync(HOMECONF)) {
-        console.log("." + CONFFILE + " not found in user HOME");
+        console.log('.' + CONFFILE + ' not found in user HOME');
         writeDefaultConfig(context);
     }
 
     // Get language server configuration and command to start server
-    let languageServerBinary =
-        vscode.workspace.getConfiguration().get('vhdlls.languageServer');
+    let languageServerBinary = vscode.workspace
+        .getConfiguration()
+        .get('vhdlls.languageServer');
     let lsBinary = languageServerBinary as keyof typeof LanguageServerBinary;
     let serverOptions: ServerOptions;
-    switch(lsBinary) {
-
-        case "embedded": 
+    switch (lsBinary) {
+        case 'embedded':
             serverOptions = getServerOptionsEmbedded(context);
             console.log('Using embedded language server');
             break;
 
-        case "user":
+        case 'user':
             serverOptions = getServerOptionsUser(context);
             console.log('Using user specified language server');
             console.log(serverOptions);
             break;
 
-        case "systemPath":
+        case 'systemPath':
             serverOptions = getServerOptionsSystemPath();
             console.log('Running language server from path');
             break;
 
-        case "docker": 
-            serverOptions = await getServerOptionsDocker(); 
+        case 'docker':
+            serverOptions = await getServerOptionsDocker();
             console.log('Using vhdl_ls from Docker Hub');
             break;
 
-        default: 
+        default:
             serverOptions = getServerOptionsEmbedded(context);
             console.log('Using language server from system path');
             break;
     }
 
-
     // Options to control the language client
     let clientOptions: LanguageClientOptions = {
         documentSelector: [{ scheme: 'file', language: 'vhdl' }],
         synchronize: {
-            // Notify the server about file changes to '.clientrc files 
+            // Notify the server about file changes to '.clientrc files
             // contained in the workspace
-            fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-        }
+            fileEvents: workspace.createFileSystemWatcher('**/.clientrc'),
+        },
     };
 
     // Create the language client
@@ -96,17 +99,17 @@ export async function activate(context: ExtensionContext) {
 
     // Register command to restart language server
     context.subscriptions.push(languageServerDisposable);
-    context.subscriptions.push(vscode.commands.registerCommand('vhdlls.restart', async () => {
-        const MSG = 'Restarting VHDL LS';
-        console.log(MSG);
-        window.showInformationMessage(MSG);
-        await client.stop();
-        languageServerDisposable.dispose();
-        languageServerDisposable = client.start();
-        context.subscriptions.push(languageServerDisposable);
-    }));
-
-
+    context.subscriptions.push(
+        vscode.commands.registerCommand('vhdlls.restart', async () => {
+            const MSG = 'Restarting VHDL LS';
+            console.log(MSG);
+            window.showInformationMessage(MSG);
+            await client.stop();
+            languageServerDisposable.dispose();
+            languageServerDisposable = client.start();
+            context.subscriptions.push(languageServerDisposable);
+        })
+    );
 }
 
 export function deactivate(): Thenable<void> | undefined {
@@ -115,7 +118,6 @@ export function deactivate(): Thenable<void> | undefined {
     }
     return client.stop();
 }
-
 
 async function getServerOptionsDocker() {
     const image = 'kraigher/vhdl_ls:latest';
@@ -130,7 +132,7 @@ async function getServerOptionsDocker() {
     let wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
     let mountPath = wsPath;
     if (isWindows) {
-        wsPath = wsPath.replace(/\\/g, "/");
+        wsPath = wsPath.replace(/\\/g, '/');
         mountPath = '/' + wsPath.replace(':', '');
     }
 
@@ -138,12 +140,17 @@ async function getServerOptionsDocker() {
     let serverArgs = [
         'run',
         '-i',
-        '-a', 'stdin',
-        '-a', 'stdout',
-        '-a', 'stderr',
+        '-a',
+        'stdin',
+        '-a',
+        'stdout',
+        '-a',
+        'stderr',
         '--rm',
-        '-w', mountPath,
-        '-v', `${wsPath}:${mountPath}:ro`,
+        '-w',
+        mountPath,
+        '-v',
+        `${wsPath}:${mountPath}:ro`,
         image,
     ];
     console.log(serverCommand);
@@ -151,64 +158,62 @@ async function getServerOptionsDocker() {
     let serverOptions: ServerOptions = {
         run: {
             command: serverCommand,
-            args   : serverArgs
+            args: serverArgs,
         },
         debug: {
             command: serverCommand,
-            args   : serverArgs
-        }
+            args: serverArgs,
+        },
     };
     return serverOptions;
 }
 
 function getServerOptionsEmbedded(context: ExtensionContext) {
-    let serverCommand =
-        context.asAbsolutePath(path.join('server', languageServerBinaryName));
+    let serverCommand = context.asAbsolutePath(
+        path.join('server', languageServerBinaryName)
+    );
     let serverOptions: ServerOptions = {
         run: {
-            command: serverCommand
+            command: serverCommand,
         },
         debug: {
-            command: serverCommand
-        }
+            command: serverCommand,
+        },
     };
     return serverOptions;
 }
-
 
 function getServerOptionsUser(context: ExtensionContext) {
-    let serverCommand : string =
-        vscode.workspace.getConfiguration().get('vhdlls.languageServerUserPath');
+    let serverCommand: string = vscode.workspace
+        .getConfiguration()
+        .get('vhdlls.languageServerUserPath');
     let serverOptions: ServerOptions = {
         run: {
-            command: serverCommand
+            command: serverCommand,
         },
         debug: {
-            command: serverCommand
-        }
+            command: serverCommand,
+        },
     };
     return serverOptions;
 }
-
 
 function getServerOptionsSystemPath() {
     let serverCommand = languageServerBinaryName;
     let serverOptions: ServerOptions = {
         run: {
-            command: serverCommand
+            command: serverCommand,
         },
         debug: {
-            command: serverCommand
-        }
+            command: serverCommand,
+        },
     };
     return serverOptions;
 }
 
-
 function writeDefaultConfig(ctx: ExtensionContext) {
-    
-    const LIBDIR = ctx.asAbsolutePath("vhdl/libraries").replace(/\\/g, "/");
-    console.log("Writing standard configuration to " + HOMECONF);
+    const LIBDIR = ctx.asAbsolutePath('vhdl/libraries').replace(/\\/g, '/');
+    console.log('Writing standard configuration to ' + HOMECONF);
     let conf = [
         `[libraries]`,
         `std.files = [`,
@@ -217,7 +222,7 @@ function writeDefaultConfig(ctx: ExtensionContext) {
         `ieee.files = [`,
         `    "${LIBDIR}/ieee_2008/*.vhdl",`,
         `    "${LIBDIR}/synopsys/*.vhdl",`,
-        `]\n`
+        `]\n`,
     ];
     fs.writeFileSync(HOMECONF, conf.join('\n'));
 }
